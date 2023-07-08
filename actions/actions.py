b9705653f8aa966044af24d6de0a42062b38f838
khetpatiparn <patiparn6mgup@gmail.com>
2023-07-09 02:08:03 +0700

    Can add more symptoms by action_ask_more_symptom

--- actions/actions.py
+++ actions/actions.py
@@ -1,12 +1,20 @@
-import requests
-
-from typing import Any, Text, Dict, List
-
+import random
+import pandas as pd
+import numpy as np
+from typing import Any, Text, Dict, List, Optional
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from rasa_sdk.events import SlotSet
-from .semantic import get_input_symptom
+from rasa_sdk.events import SlotSet, UserUtteranceReverted, AllSlotsReset
+from .semantic import get_input_symptom, embed_text
+import ast
+
+"""# Data loading"""
+data = pd.read_csv("/home/patiparn/rasa_thainlp/rasatest/actions/sepSymptom.csv")
+data["symptom"] = data["symptom1"].apply(ast.literal_eval)
+data['embed'] = [np.array(embed_text(text)) for text in data['symptom']]
+
+SYMPTOMS_DATABASE = []
 
 class ActionAskSymptom(Action):
 
@@ -17,29 +25,29 @@ class ActionAskSymptom(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        symptoms = tracker.get_slot("symptom") # รับ slot 
-        
+        symptoms = tracker.get_slot("symptom") # รับ slot => ["ปวดหัว", "ตัวร้อน"]
+
         if not symptoms: # ในตอนแรก slot จะไม่มีการเก็บค่า เริ่มต้นเป็น None เราก็ให้แสดงข้อความอื่นแทน
             dispatcher.utter_message(text="เริ่มบอกอาการของคุณมาได้เลยย")
-        
-        else: # หลังจากได้ค่าที่มาจาก slots แล้วเช่น ["ปวดหัว", "ตัวร้อน"]
-            
+
+        else: # หลังจากได้ค่าที่มาจาก slots 
             # 1. เรียกใช้งานฟังก์ชัน search(symptoms) จากไฟล์ semantic
-            result_from_input = get_input_symptom(symptoms)
-            '''
-            {62: {'name': 'Common cold', 'th_name': 'ไข้หวัด', 'score': 0.7517}, 141: {'name':                                    
-            'Norovirus Infection', 'th_name': 'การติดเชื้อท้องเสียโนโรไวรัส (Norovirus)',                                         
-            'score': 0.7351}, 180: {'name': 'Systemic Lupus Erythematosus (SLE)', 'th_name':                                      
-            'เอสแอลอี (SLE) - โรคแพ้ภูมิตัวเอง - ลูปัส', 'score': 0.7058}, 198: {'name': 'Zika                                    
-            Virus and Pregnancy', 'th_name': 'โรคไข้ซิกากับการตั้งครรภ์', 'score': 0.6892}} 
-            '''
+            result_from_input = get_input_symptom(symptoms, data)
+
             # 2 คัดแยกข้อมูลที่ได้
-            message = FilterData(result_from_input, theshold = 85)
-            dispatcher.utter_message(text="{}".format(message))
+            item_or_message, is_valid= FilterData(result_from_input, symptoms, theshold = 76)
+            if (is_valid == True):
+                dispatcher.utter_message(text="{}".format(item_or_message))
             
+            elif (is_valid == False):
+                dispatcher.utter_message(text="{}".format(item_or_message))
         return []
 
-def FilterData(result_from_input, theshold):
+# ฉันรู้สึกปวดหัวและตัวร้อนเล็กน้อย
+# ฉันอยากทราบอาการของฉัน
+# ฉันรู้สึกเจ็บคอ
+
+def FilterData(result_from_input,symptoms, theshold):
     # 1 กรองตัดเลขด้านหน้าออก
     for idx, item in enumerate(result_from_input.values()):
 
@@ -47,12 +55,37 @@ def FilterData(result_from_input, theshold):
     # 2.1 ถ้าหาก score มากกว่า theshold ให้คืนข้อมูลที่มีค่า score สูงที่สุด
         if (item["score"] * 100) >=  theshold:
             if idx == 0:
-                return item
+                message = "คุณน่าจะมีอาการ "
+                return message + item["th_name"], True
             
     # 2.2 ถ้าหาก score น้อยกว่า theshold ให้คืน message กลับไปที่ user ว่าขอข้อมูลเพิ่ม
         elif (item["score"] * 100) < theshold:
-            message = "เราขอทราบอาการของคุณเพิ่มเติมเพื่อการวิเคราะห์ผลที่แม่นยำขึ้น."
-            return message
+            message = message_for_more_symptoms(symptoms)
+            return message, False
+        
+def message_for_more_symptoms(symptoms):
+    concat = ", ".join(symptoms)
+    messages = [
+        "คุณมีอาการอะไรอื่นร่วมด้วยหรือไม่",
+        "นอกจาก {} แล้วมีอาการอื่นอีกไหม".format(concat)
+            ]
+    random_message = random.choice(messages)
+    return random_message
 
-# ฉันรู้สึกปวดหัวและตัวร้อนเล็กน้อย
-# ฉันอยากทราบอาการของฉัน
+
+class ActionAskMoreSymptom(Action):
+
+    def name(self) -> Text:
+        return "action_ask_more_symptom"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        entities = tracker.get_latest_entity_values("symptom")
+        myvalue = list(entities) # ['หน้าแดง']
+        slots_current = tracker.get_slot("symptom") # ['ปวดหัว', 'ตัวร้อน']
+        total_symptom = slots_current + myvalue
+        dispatcher.utter_message(text="มี{}ร่วมด้วยนะครับ".format(','.join(myvalue)))
+        return [SlotSet(key = "symptom", value = total_symptom)]
+
--- actions/semantic.py
+++ actions/semantic.py
@@ -32,19 +32,11 @@ def find_similar_vectors(target_vector, vectorss, input_len):
     
     return match_items
 
-def get_input_symptom(input):
-
-    """# Data loading"""
-
-    data = pd.read_csv("/home/patiparn/rasa_thainlp/rasatest/actions/sepSymptom.csv")
-    data["symptom"] = data["symptom1"].apply(ast.literal_eval)
-    data['embed'] = [np.array(embed_text(text)) for text in data['symptom']]
+def get_input_symptom(input,data):
     """# search"""
 
     input_len = len(input)
-    embedding_input = np.array(embed_text(input))
-    target_vector = embedding_input
-
+    target_vector = np.array(embed_text(input))
     vectors = data['embed'].values
 
     match_item = find_similar_vectors(target_vector, vectors, input_len)
@@ -56,8 +48,6 @@ def get_input_symptom(input):
     symptom_num = [item["symptom_num"] for item in match_item]
     idx = [item["idx"] for item in match_item]
 
-    # sim_data = data[["th_name","len","symptom"]].iloc[idx]
-
 
     # Convert list of sets to a single list of integers
     int_list = []
@@ -66,8 +56,7 @@ def get_input_symptom(input):
     selected_symp = [data['symptom'][id_val][id_sym] for id_val,id_sym in zip(idx_repos,int_list)]
 
     embed_sim_data = embed_text(selected_symp)
-
-    cos_sim = cosine_similarity(embed_sim_data,embedding_input)
+    cos_sim = cosine_similarity(embed_sim_data, target_vector)
     max_sim = np.max(cos_sim, axis=1)
 
 
@@ -90,10 +79,13 @@ def get_input_symptom(input):
         output_dict[id] = {"name":data["name"][id],
                         "th_name":data["th_name"][id],
                             "score":round(socre[i],4)}
-        
     print("...ประมวลผลเสร็จสิ้น...")
     return output_dict
 
 if __name__ == "__main__":
-    symptoms = ['ปวดหัว', 'ตัวร้อน']
-    print(get_input_symptom(symptoms))
+    """# Data loading"""
+    # data = pd.read_csv("/home/patiparn/rasa_thainlp/rasatest/actions/sepSymptom.csv")
+    # data["symptom"] = data["symptom1"].apply(ast.literal_eval)
+    # data['embed'] = [np.array(embed_text(text)) for text in data['symptom']]
+    # symptoms = ['ปวดหัว', 'ตัวร้อน']
+    # print(get_input_symptom(symptoms,data))
